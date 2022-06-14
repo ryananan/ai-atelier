2133a3a5da5e72d7e46773ec62f3fa2a1e8ec148
Muhan Xu <53051442+ryananan@users.noreply.github.com>
2022-06-15 03:37:11 +0800

    debug mode off

--- ai-atelier-cn.py
+++ ai-atelier-cn.py
@@ -22,9 +22,9 @@ from kora.xattr import get_id
 import deepl
 import translators as ts
 
-# from setup import textsynth_completion
+from setup import textsynth_completion
 # To debug on mac
-from setup_mac import textsynth_completion
+# from setup_mac import textsynth_completion
 
 torch.cuda.empty_cache()
 
